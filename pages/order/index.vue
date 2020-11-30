75844002d437d3f8725814b835a723648ff3a160
辣个男人 <1374636431@qq.com>
2020-11-30 13:39:21 +0800

    z2

--- pages/order/index.vue
+++ pages/order/index.vue
@@ -2,17 +2,21 @@
 	<view class="container">
 		<view class="bannerBox">
 			<view class="tabsBox">
-				<text :class="index==i?'tab active':'tab'" v-for="(tab,i) in tabs" :key="i">{{tab}}</text>
+				<text :class="index==i?'tab active':'tab'" v-for="(tab,i) in tabs" :key="i" @click="index=i">{{tab}}</text>
 			</view>
 			<view class="optBox">
-				
+				<image src="/static/image/sort.png" mode="" class="icon"></image>
 			</view>
 		</view>
+		<chunLei-popups  :popData="data" @tapPopup="tapPopup" :x="344" :y="20" placement="top-end">
+		</chunLei-popups>
 	</view>
 </template>
 
 <script>
+	import chunLeiPopups from "@/components/chunLei-popups/chunLei-popups.vue";
 	export default {
+		components:{chunLeiPopups},
 		data() {
 			return {
 				tabs: ['分拣接单', '待分拣', '已分拣', '已退单'],
@@ -25,9 +29,10 @@
 <style lang="scss" scoped>
 	.bannerBox {
 		background: #fff;
-		padding: 20rpx 0;
-
+		
+        display: flex;
 		.tabsBox {
+			padding: 20rpx 0;
 			padding-left: 32rpx;
            width: 568rpx;
 		   overflow-x: scroll;
@@ -52,6 +57,18 @@
 			.tab.active {
 				color: #181819;
 				background: #FFAE18;
+			}
+		}
+		.optBox{
+			background: #fff;
+			padding:0 25rpx;
+			display: flex;
+			align-items: center;
+			height: 100%;
+box-shadow: -6px 0px 8px 0px rgba(0,0,0,0.1);
+			.icon{
+				width: 52rpx;
+				height: 52rpx;
 			}
 		}
 	}
